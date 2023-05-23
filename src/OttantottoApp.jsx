b90825bccf592da8565eac583fd553c3b18dd923
luhobrecht <luhobrecht@hotmail.com>
2023-05-23 16:51:55 +0200

    Project initialization

---
+++ src/OttantottoApp.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { NavBar } from './components/NavBar/NavBar'
+
+function OttantottoApp() {
+  return (
+    <>   
+    <NavBar />
+    </>
+  )
+}
+
+export default OttantottoApp
---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,10 @@
+
+export const Logo = () => {
+  return (
+    <>
+        <div>
+            <img src="https://i.imgur.com/8XZQ1ZT.png" alt="logo" />
+        </div>
+    </>
+  )
+}
