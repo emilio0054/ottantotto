abb5194b42b2e1e50b59a6121c1cfe3098726903
luhobrecht <luhobrecht@hotmail.com>
2023-05-25 17:13:48 +0200

    working on menu view

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,7 +2,14 @@
 export const Menu = () => {
   return (
     <>
-        
+      <div className="img-container">
+        <img src="/assets/menu/1spa.png" alt='Menú en Español' id='menu-img' width='130%'/>
+        <hr />
+        <img src="/assets/menu/2spa.png" alt='Menú en Español' id='menu-img' width='115%'/>
+        <hr />
+        <img src="/assets/menu/3spa.png" alt='Menú en Español' id='menu-img' width='100%' />
+        <hr />
+      </div>
     </>
   )
 }
