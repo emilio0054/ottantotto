9806c174f87bc7b1c6bd076890881d803543db44
luhobrecht <luhobrecht@hotmail.com>
2023-05-26 13:28:14 +0200

    changes in home view and navbar

--- src/components/HomeView/HomeView.jsx
+++ src/components/HomeView/HomeView.jsx
@@ -1,19 +1,14 @@
+import { Link, NavLink } from "react-router-dom";
+
 export const HomeView = () => {
   return (
     <>
-      <div>
-        <div className="row">
-          <div className="col-12">
-            <img className="homeview-image" src="/tomates.jpg" width='100%'/>
-            <h1 className="homeview-text-center">¡Bienvenidos a Ottantotto!</h1>
-            <a
-              classname="homeview-adress"
-              href="https://goo.gl/maps/kEcNZSV8wWY8TKst6?coh=178571&entry=tt"
-            >
-              <h5>Passatge d'Utset, 2, 08013 Barcelona</h5>
-            </a>
-          </div>
-        </div>
+      <div className="d-flex flex-column justify-content-center align-items-center homeview-container">
+          <h1 className="homeview-text">¡Bienvenidos a Ottantotto!</h1>
+          <NavLink  to='https://goo.gl/maps/kEcNZSV8wWY8TKst6?coh=178571&entry=tt' classname="mt-2" target="_blank" id='homeview-adress'
+          >Passatge d'Utset, 2, 08013, Barcelona
+          </NavLink>
+          <Link to='tel:930381911' id='homeview-phone' className="mt-2">930381911</Link>
       </div>
     </>
   );
