768a13c86bdb5d6d250acef4d7cc6a6ceab126d0
luhobrecht <luhobrecht@hotmail.com>
2023-06-07 17:54:48 +0200

    changes in footer and contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,7 +7,7 @@ export const Contact = ({id}) => {
     <div className="contact-container" id={id}>
      <h2 className="d-flex justify-content-center contact-title">Reservas y pedidos</h2> 
      <p className="d-flex justify-content-center contact-subtitle">Haz tu reserva con tan sólo un click</p> 
-     <div className="d-flex justify-content-evenly main-container">
+     <div className="d-flex justify-content-evenly main-container container">
         <div className="d-flex flex-column justify-content-center">
           <a href='https://api.whatsapp.com/send?phone=34722194408&text=¡Hola!%20quiero%20hacer%20una
 %20reserva:%20(incluye%20cantidad%20de%20personas,%20día%20y%20hora%20en%20tu%20solicitud.)' target='_blank' className="text-decoration-none text-center">
@@ -32,7 +32,7 @@ export const Contact = ({id}) => {
         </div>
      </div>
      <p className="contact-confirmation">Confirmaremos su reserva o pedido tan pronto comprobemos la disponibilidad para la fecha y hora solicitadas.</p> 
-     <p className="contact-confirmation mt-0 mb-5">Si la reserva o pedido no son confirmados, no tendrá validez alguna.</p>
+     <p className="contact-confirmation mt-0 mb-2">Si la reserva o pedido no son confirmados, no tendrá validez alguna.</p>
     </div>
     </>
   )
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -7,7 +7,7 @@ export const Menu = ({id}) => {
         <div className='d-flex justify-content-start mt-4 mb-5'>
         <img src="/assets/menu/1spa.png" alt='Menú en Español' id='menu-img' width='30%' />
         </div>
-        <div className='d-flex justify-content-evenly menu-container'>
+        <div className='d-flex justify-content-evenly bottom-menu-container'>
         <img src="/assets/menu/2spa.png" alt='Menú en Español' id='menu-img' width='40%'/>
         <hr className='d-none'/>
         <img src="/assets/menu/3spa.png" alt='Menú en Español' id='menu-img' width='40%' />
