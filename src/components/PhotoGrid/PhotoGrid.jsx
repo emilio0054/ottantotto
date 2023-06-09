02b18e98074e116ebeb13f743163af605b064b9f
emilio0054 <leonemilio@gmail.com>
2023-06-09 14:00:06 +0200

    z-pattern gallery, add more photos

--- src/components/PhotoGrid/PhotoGrid.jsx
+++ src/components/PhotoGrid/PhotoGrid.jsx
@@ -2,67 +2,148 @@ import "./photoGrid.css";
 export const PhotoGrid = () => {
   return (
     <div>
-      <div className="grid-container">
-        <div className="grid-item">
-          <img
-            src="/assets/PIZZA ROJA/pizza1.jpg"
-            width="750px"
-            height="750px"
-            alt="Image 1"
-          />
+      <section class="section-zpattern">
+        <div class="container">
+          <h2 class="heading-secondary">Lo que hacemos, lo hacemos con amor</h2>
         </div>
-        <div className="grid-item resize">
-          <img
-            src="/assets/downloaded/deniz-altindas-jVLahCBXaJs-unsplash.jpg"
-            width="750px"
-            height="750px"
-            alt="Image 2"
-          ></img>
+
+        <div class="container grid--2-cols grid">
+          <div class="text">
+            <p class="description">La masa</p>
+            <h3 class="step-description">
+              nuestra masa, de leudado lento, está hecha con harina de trigo Bio
+              italiana. Una masa bien hecha es simplemente hermosa. El resultado
+              es una pizza que es leve al cuerpo del comensal, pero
+              profundamente gratificante.
+            </h3>
+          </div>
+          <div class="gallery">
+            <img
+              src="/assets/z pattern/01 La masa/masa1.png"
+              class="step-img"
+              alt="masa1"
+            />
+            <img
+              src="/assets/z pattern/01 La masa/masa2.png"
+              class="step-img"
+              alt="masa2"
+            />
+            <img
+              src="/assets/z pattern/01 La masa/masa3.jpg"
+              class="step-img"
+              alt="masa3"
+            />
+            <img
+              src="/assets/z pattern/01 La masa/masa4.jpg"
+              class="step-img"
+              alt="masa4"
+            />
+          </div>
         </div>
-        <div className="grid-item resize">
-          <img
-            src="/assets/downloaded/aliona-gumeniuk-CWAargZlesM-unsplash.jpg"
-            width="750px"
-            height="750px"
-            alt="Image 3"
-          ></img>
+
+        <div class="container grid--2-cols grid">
+          <div class="gallery">
+            <img
+              src="/assets/z pattern/02 El tomate/tomate1.jpg"
+              class="step-img"
+              alt="tomate1"
+            />
+            <img
+              src="/assets/z pattern/02 El tomate/tomate2.jpg"
+              class="step-img"
+              alt="tomatea2"
+            />
+            <img
+              src="/assets/z pattern/02 El tomate/tomate3.jpg"
+              class="step-img"
+              alt="tomate3"
+            />
+            <img
+              src="/assets/z pattern/02 El tomate/tomate4.jpg"
+              class="step-img"
+              alt="tomate4"
+            />
+          </div>
+          <div>
+            <p class="description">El tomate</p>
+            <h3 class="step-description">
+              hacemos la salsa con tomates italianos de primera calidad. Cocida
+              al horno, su perfume es incunfundible.
+            </h3>
+          </div>
         </div>
-        <div className="grid-item">
-          <img
-            src="/assets/PIZZA ROJA/pizza4.jpg"
-            width="750px"
-            height="750px"
-            alt="Image 4"
-          ></img>
+
+        <div class="container grid--2-cols grid">
+          <div>
+            <p class="description">La mozzarella</p>
+            <h3 class="step-description">
+              La mozzarella de búfala es la mejor opción para nuestras pizzas.
+              Hecha con leche fresca de búfala, tiene un sabor suave y cremoso.
+              Su textura elástica y capacidad para fundirse la convierten en el
+              complemento perfecto para nuestra pizza deliciosa. Utilizarla
+              garantiza una experiencia culinaria auténtica y de alta calidad.
+            </h3>
+          </div>
+          <div class="gallery">
+            <img
+              src="/assets/z pattern/03 la mozzarella/lamuzza1.jpg"
+              class="step-img"
+              alt="muzza1"
+            />
+            <img
+              src="/assets/z pattern/03 la mozzarella/lamuzza2.jpg"
+              class="step-img"
+              alt="muzza2"
+            />
+            <img
+              src="/assets/z pattern/03 la mozzarella/lamuzza3.jpg"
+              class="step-img"
+              alt="muzza3"
+            />
+            <img
+              src="/assets/z pattern/03 la mozzarella/lamuzza4.jpg"
+              class="step-img"
+              alt="muzza4"
+            />
+          </div>
         </div>
-        <div className="grid-item resize">
-          <img
-            src="/assets/downloaded/dorien-monnens-42zN6Txav3w-unsplash.jpg"
-            width="750px"
-            height="750px"
-            alt="Image 5"
-          ></img>
+
+        <div class="container grid--2-cols grid">
+          <div class="gallery">
+            <img
+              src="public/assets/z pattern/04 Los ingredientes/ingredientes1.jpg"
+              class="step-img"
+              alt="ingredientes1"
+            />
+            <img
+              src="public/assets/z pattern/04 Los ingredientes/ingredientes2.jpg"
+              class="step-img"
+              alt="ingredientes2"
+            />
+            <img
+              src="public/assets/z pattern/04 Los ingredientes/ingredientes3.jpg"
+              class="step-img"
+              alt="ingredientes3"
+            />
+            <img
+              src="public/assets/z pattern/04 Los ingredientes/ingredientes4.jpg"
+              class="step-img"
+              alt="ingredientes4"
+            />
+          </div>
+          <div>
+            <p class="description">Ingredientes</p>
+            <h3 class="step-description">
+              sólo utilizamos ingredientes de primera, importados de Italia o de
+              producción local. Sin atajos.Además de las tradicionales Marinara
+              y Margherita, hemos diseñado un buen número de pizzas honrando a
+              diversas regiones de Italia. Cada una cuenta con algún ingrediente
+              representativo de su propia riqueza gastronómica, directamente
+              importado de origen.
+            </h3>
+          </div>
         </div>
-        <div className="grid-item">
-          <img src="/assets/PIZZA BLANCA/pizzab1.jpg" alt="Image 6"></img>
-        </div>
-        <div className="grid-item">
-          <img
-            src="/assets/PIZZA BLANCA/pizzab2.jpg"
-            width="750px"
-            height="750px"
-            alt="Image 7"
-          ></img>
-        </div>
-        <div className="grid-item">
-          <img
-            src="/assets/downloaded/vaishnav-chogale-cvfxrmFYkZ8-unsplash.jpg"
-            width="750px"
-            height="750px"
-            alt="Image 8"
-          ></img>
-        </div>
-      </div>
+      </section>
     </div>
   );
 };
