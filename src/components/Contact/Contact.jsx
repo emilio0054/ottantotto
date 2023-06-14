00756f0f10bebd223d318c33f2e799275c85975b
luhobrecht <luhobrecht@hotmail.com>
2023-06-14 13:17:56 +0200

    changes in maps

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -11,21 +11,21 @@ export const Contact = ({id}) => {
           <a href='https://api.whatsapp.com/send?phone=34722194408&text=¡Hola!%20quiero%20hacer%20una
 %20reserva:%20(incluye%20cantidad%20de%20personas,%20día%20y%20hora%20en%20tu%20solicitud.)' target='_blank' className="text-decoration-none text-center">
           <i className="bi bi-whatsapp whatsapp-icon"></i>
-          <h3>Por Whatsapp</h3>
+          <h3 className="contact-text">Por Whatsapp</h3>
           </a>
           <p>Horario de atención: </p>
         </div>
         <div className="d-flex flex-column justify-content-center w-25">
           <a href='tel:930381911' target='_blank' className="text-decoration-none text-center">
           <i className="bi bi-telephone-outbound phone-icon"></i>
-          <h3>Por teléfono</h3>
+          <h3 className="contact-text">Por teléfono</h3>
           </a>
           <p>Horario de atención: </p>
         </div>
         <div className="d-flex flex-column justify-content-center w-25">
           <a href='mailto:' target='_blank' className="text-decoration-none text-center">
           <i className="bi bi-envelope-at mail-icon"></i>
-          <h3 className="" >Envíanos un mail</h3>
+          <h3 className="contact-text" >Envíanos un mail</h3>
           </a>
           <p className="mail-text"> Info acá</p>
         </div>
--- src/components/Maps/Maps.jsx
+++ src/components/Maps/Maps.jsx
@@ -1,27 +1,58 @@
-import { GoogleMap, withScriptjs, withGoogleMap, Marker } from 'react-google-maps';
-
-function Map() {
-  return (
-    <GoogleMap
-      defaultZoom={12}
-      defaultCenter={{ lat: 40.7128, lng: -74.0060 }} // Cambia las coordenadas según tu ubicación
-    >
-      <Marker position={{ lat: 40.7128, lng: -74.0060 }} title="¡Aquí estoy!" />
-    </GoogleMap>
-  );
-}
-
-const WrappedMap = withScriptjs(withGoogleMap(Map));
+import React, { useEffect, useRef, useState } from 'react';
 
 export function Maps() {
-  return (
-    <div style={{ width: '100%', height: '400px' }}>
-      <WrappedMap
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyDapB6-c-4-j_hlUtlXqto4oGFSBi-AYRs`}
-        loadingElement={<div style={{ height: '100%' }} />}
-        containerElement={<div style={{ height: '100%' }} />}
-        mapElement={<div style={{ height: '100%' }} />}
-      />
-    </div>
-  );
+  const mapContainerRef = useRef(null);
+  const [map, setMap] = useState(null);
+
+  useEffect(() => {
+    const loadMap = () => {
+      const google = window.google;
+
+      const mapOptions = {
+        center: { lat: 41.404770221563545, lng: 2.177465510753392 }, // Coordenadas de ubicación predeterminadas  
+        zoom: 16,
+      };
+
+      const mapElement = mapContainerRef.current;
+      const newMap = new google.maps.Map(mapElement, mapOptions);
+
+      const geocoder = new google.maps.Geocoder();
+      const address = "Passatge d'Utset, 2, 08013 Barcelona";
+
+      geocoder.geocode({ address }, (results, status) => {
+        if (status === google.maps.GeocoderStatus.OK) {
+          const location = results[0].geometry.location;
+          newMap.setCenter(location);
+
+          const marker = new google.maps.Marker({
+            position: location,
+            map: newMap,
+            title: address,
+          });
+        } else {
+          console.error('Geocodificación fallida. Error:', status);
+        }
+      });
+
+      setMap(newMap);
+    };
+
+    const loadGoogleMapsScript = () => {
+      if (!window.google) {
+        const googleMapScript = document.createElement('script');
+        googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDapB6-c-4-j_hlUtlXqto4oGFSBi-AYRs&callback=initMap`;
+        googleMapScript.async = true;
+        googleMapScript.defer = true;
+        window.initMap = loadMap;
+        document.head.appendChild(googleMapScript);
+      } else {
+        loadMap();
+      }
+    };
+
+    loadGoogleMapsScript();
+  }, []);
+
+  return <div ref={mapContainerRef} style={{ width: '100%', height: '400px' }} />;
 }
+
