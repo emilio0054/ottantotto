9813e6e51ff2f3dabd7cd0a0bc5ad3db7683d6bf
luhobrecht <luhobrecht@hotmail.com>
2023-06-15 18:44:44 +0200

    styles in photogrid and menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -4,13 +4,13 @@ export const Menu = ({id}) => {
   return (
     <>
       <div className="menu-container" id={id}>
-        <div className='d-flex justify-content-start mt-4 mb-5'>
-        <img src="/assets/menu/1spa.png" alt='Menú en Español' id='menu-img' width='30%' />
+        <div className='d-flex justify-content-center mt-4 mb-5'>
+        <img src="/assets/menu/1spa.png" alt='Menú en Español' id='menu-img' width='30%' className='logo-photo' />
         </div>
         <div className='d-flex justify-content-evenly bottom-menu-container'>
-        <img src="/assets/menu/2spa.png" alt='Menú en Español' id='menu-img' width='40%'/>
+        <img src="/assets/menu/2spa.png" alt='Menú en Español' id='menu-img' width='45%'/>
         <hr className='d-none'/>
-        <img src="/assets/menu/3spa.png" alt='Menú en Español' id='menu-img' width='40%' />
+        <img src="/assets/menu/3spa.png" alt='Menú en Español' id='menu-img' width='45%' />
         </div>
       </div>
     </>
--- src/components/PhotoGrid/PhotoGrid.jsx
+++ src/components/PhotoGrid/PhotoGrid.jsx
@@ -95,6 +95,34 @@ export const PhotoGrid = () => {
                 />
             </div>
         </div>
+        <div className='row row-cols-2 justify-content-between mb-5'>
+            <div className="row row-cols-2 image-container rounded gx-2 gy-2">
+                <img
+                src="/assets/z-pattern/01 La masa/masa1.png"
+                className="col image rounded"
+                alt="masa1"
+                />
+                <img
+                src="/assets/z-pattern/01 La masa/masa2.png"
+                className="col image rounded"
+                alt="masa2"
+                />
+                <img
+                src="/assets/z-pattern/01 La masa/masa3.jpg"
+                className="col image rounded"
+                alt="masa3"
+                />
+                <img
+                src="/assets/z-pattern/01 La masa/masa4.jpg"
+                className="col image rounded"
+                alt="masa4"
+                />
+            </div>
+            <div className='col-4 align-self-center'>
+                <h2>Los ingredientes</h2>
+                <p>Sólo utilizamos ingredientes de primera, importados de Italia o de producción local. Sin atajos.</p>
+            </div>
+        </div>
     </div>
   )
 }
