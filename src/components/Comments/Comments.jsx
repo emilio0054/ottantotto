2e9f1d94e715c5366a12e1cc9bfac48a72a71568
emilio0054 <leonemilio@gmail.com>
2023-06-29 16:58:50 +0200

    changes in comments

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -2,25 +2,85 @@ import "./comments.css";
 
 export const Comments = () => {
   return (
-    <div class="container">
-      <div class="">
-        <div class="col-12 vertical-center">
-          <h1>hola aca comentarios</h1>
-          <p>
-            .. no sé si se puede copiar y pegar una foto del comentario. O si
-            hay que tener un link de la pagina del comentario para importarlo..
-            ye ne se pá! La realidad que vi comentarios muy buenos en todas las
-            redes de restaurantes que hay. Tripadvisor, the fork (aunque arturo
-            la odia), restaurant guru, google maps. tal vez estaria bueno poner
-            un logo de{" "}
-            <a
-              href="https://www.tripadvisor.com/Restaurant_Review-g187497-d13763198-Reviews-Pizzeria_Bar_Ottantotto-Barcelona_Catalonia.html#REVIEWS"
-              target="_blank"
-            >
-              Tripadvisor
-            </a>
-          </p>
+    <div
+      id="carouselExampleControls"
+      class="carousel slide"
+      data-bs-ride="carousel"
+    >
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <div class="testimonials-container">
+            <h1 class="titulo">Testimonios</h1>
+            <div class="testimonials">
+              <figure class="testimonial">
+                <div class="testimonialimg">
+                  <img
+                    class="testimonial-img"
+                    alt="Photo of customer jonrwilliams"
+                    src="/assets/testimonials/jonrwilliams.jpg"
+                  />
+                  <img
+                    class="testimonial-logo"
+                    alt="tripadvisor logo"
+                    src="/assets/testimonials/tripadvisor-logo.png"
+                  />
+                </div>
+                <blockquote class="testimonial-text">
+                  "A taste of Italy in Barcelona Stopped here for a little taste
+                  of Italy in Barcelona. Service was very personal and the food
+                  was excellent. Highly recommend if you're looking for
+                  alternative to local cuisine."
+                </blockquote>
+                <p class="testimonial-name">&mdash; jonrwilliams</p>
+              </figure>
+
+              <figure class="testimonial">
+                <div class="testimonialimg">
+                  <img
+                    class="testimonial-img"
+                    alt="Photo of customer tnegro"
+                    src="/assets/testimonials/tnegro.jpg"
+                  />
+                  <img
+                    class="testimonial-logo"
+                    alt="tripadvisor logo"
+                    src="/assets/testimonials/tripadvisor-logo.png"
+                  />
+                </div>
+                <blockquote class="testimonial-text">
+                  "Great pizza and friendly staff! Choosing this place for good
+                  pizza was right decision. Friendly owner with some good
+                  recommendations make us great lunch!"
+                </blockquote>
+                <p class="testimonial-name">&mdash; tnegro</p>
+              </figure>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="..." class="d-block w-100" alt="..."></img>
+          </div>
+          <div class="carousel-item">
+            <img src="..." class="d-block w-100" alt="..."></img>
+          </div>
         </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
       </div>
     </div>
   );
