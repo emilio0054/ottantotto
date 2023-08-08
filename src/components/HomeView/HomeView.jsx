e5c18ee1deb896802fd143ab8a0a9615bc1423c1
luhobrecht <luhobrecht@hotmail.com>
2023-08-08 21:04:56 +0200

    fixed homeview

--- src/components/HomeView/HomeView.jsx
+++ src/components/HomeView/HomeView.jsx
@@ -13,49 +13,49 @@ export const HomeView = ({ id }) => {
             <div className="carousel-item active" data-bs-interval="3000">
               <img
                 src="/assets/homeview/homeview8.jpeg"
-                className="d-block w-100 img-fluid"
+                className="d-block w-100"
                 alt="pizza piemonte"
               />
             </div>
             <div className="carousel-item" data-bs-interval="3000">
               <img
                 src="/assets/homeview/homeview6.jpg"
-                className="d-block w-100 img-fluid"
+                className="d-block w-100 "
                 alt="flores de calabaza"
               />
             </div>
             <div className="carousel-item" data-bs-interval="3000">
               <img
                 src="/assets/homeview/homeview9.jpeg"
-                className="d-block w-100 img-fluid"
+                className="d-block w-100 "
                 alt="pizza liguria"
               />
             </div>
             <div className="carousel-item" data-bs-interval="3000">
               <img
                 src="/assets/homeview/homeview10.jpeg"
-                className="d-block w-100 img-fluid"
+                className="d-block w-100 "
                 alt="pizza sardegna"
               />
             </div>
             <div className="carousel-item" data-bs-interval="3000">
               <img
                 src="/assets/homeview/homeview12.jpg"
-                className="d-block w-100 img-fluid"
+                className="d-block w-100 "
                 alt="tomate con albahaca"
               />
             </div>
             <div className="carousel-item" data-bs-interval="3000">
               <img
                 src="/assets/homeview/homeview11.jpeg"
-                className="d-block w-100 img-fluid"
+                className="d-block w-100 "
                 alt="pizza calabria"
               />
             </div>
             <div className="carousel-item" data-bs-interval="3000">
               <img
                 src="/assets/homeview/homeview3.jpg"
-                className="d-block w-100 img-fluid"
+                className="d-block w-100 "
                 alt="postre tiramisu"
               />
             </div>
