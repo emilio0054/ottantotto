bb93674b300cf85991bd0d504427528889bdc0e2
emilio0054 <leonemilio@gmail.com>
2024-01-23 17:58:37 +0100

    change in contactus, change in map

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -11,55 +11,57 @@ export const Contact = ({ id }) => {
           MAKE YOUR BOOKING WITH ONE CLICK
         </p>
         <div className="d-flex justify-content-evenly main-container container h-auto">
-          <div className="col-lg-2 col-sm-4 d-flex flex-column justify-content-space-around align-content-center item">
-            <a
-              href="https://api.whatsapp.com/send?phone=34722194408&text=¡Hola!%20quiero%20hacer%20una
+          <div className="row">
+            <div className="col-xs-12 col-sm-6 col-lg-3 d-flex flex-column justify-content-space-around align-content-center item">
+              <a
+                href="https://api.whatsapp.com/send?phone=34722194408&text=¡Hola!%20quiero%20hacer%20una
   %20reserva:%20(incluye%20cantidad%20de%20personas,%20día%20y%20hora%20en%20tu%20solicitud.)"
-              target="_blank"
-              className="text-decoration-none text-center"
-            >
-              <i className="bi bi-whatsapp whatsapp-icon"></i>
-              <h3 className="contact-text">Whatsapp</h3>
-            </a>
-            <p className="mb-0">Monday to Friday</p>
-            <p className="mt-0 mb-0">12:00 a 20:00hs</p>
-            <p className="statement">
-              *Bookings must be made at least 24 hours in advance.
-            </p>
-          </div>
-          <div className="col-lg-2 col-sm-4 d-flex flex-column justify-content-space-around align-content-center item">
-            <a
-              href="tel:930381911"
-              target="_blank"
-              className="text-decoration-none text-center"
-            >
-              <i className="bi bi-telephone-outbound phone-icon"></i>
-              <h3 className="contact-text">Phone</h3>
-            </a>
-            <p className="mb-0">Monday to Sunday</p>
-            <p className="mt-0 mb-0">12:00 a 22:00</p>
-          </div>
-          <div className="col-lg-2 col-sm-4 d-flex flex-column justify-content-space-around align-content-center item">
-            <a
-              href="https://goo.gl/maps/wBcuYJXGbrhH2tdc9"
-              target="_blank"
-              className="text-decoration-none text-center"
-            >
-              <i className="bi bi-geo-alt-fill location-icon"></i>
-              <h3 className="contact-text">Location</h3>
-            </a>
-            <p className="location-text">Info</p>
-          </div>
-          <div className="col-lg-2 col-sm-4 d-flex flex-column justify-content-space-around align-content-center item">
-            <a
-              href="mailto:pizzeriaottantotto@gmail.com"
-              target="_blank"
-              className="text-decoration-none text-center"
-            >
-              <i className="bi bi-envelope-at mail-icon"></i>
-              <h3 className="contact-text">Send us an Email!</h3>
-            </a>
-            <p className="mail-text">Info</p>
+                target="_blank"
+                className="text-decoration-none text-center"
+              >
+                <i className="bi bi-whatsapp whatsapp-icon"></i>
+                <h3 className="contact-text">Whatsapp</h3>
+              </a>
+              <p className="mb-0">Monday to Friday</p>
+              <p className="mt-0 mb-0">12:00 a 20:00hs</p>
+              <p className="statement">
+                *Bookings must be made at least 24 hours in advance.
+              </p>
+            </div>
+            <div className="col-xs-12 col-sm-6 col-lg-3 d-flex flex-column justify-content-space-around align-content-center item">
+              <a
+                href="tel:930381911"
+                target="_blank"
+                className="text-decoration-none text-center"
+              >
+                <i className="bi bi-telephone-outbound phone-icon"></i>
+                <h3 className="contact-text">Phone</h3>
+              </a>
+              <p className="mb-0">Monday to Sunday</p>
+              <p className="mt-0 mb-0">12:00 a 22:00</p>
+            </div>
+            <div className="col-xs-12 col-sm-6 col-lg-3 d-flex flex-column justify-content-space-around align-content-center item">
+              <a
+                href="https://goo.gl/maps/wBcuYJXGbrhH2tdc9"
+                target="_blank"
+                className="text-decoration-none text-center"
+              >
+                <i className="bi bi-geo-alt-fill location-icon"></i>
+                <h3 className="contact-text">Location</h3>
+              </a>
+              <p className="location-text">Info</p>
+            </div>
+            <div className="col-xs-12 col-sm-6 col-lg-3 d-flex flex-column justify-content-space-around align-content-center item">
+              <a
+                href="mailto:pizzeriaottantotto@gmail.com"
+                target="_blank"
+                className="text-decoration-none text-center"
+              >
+                <i className="bi bi-envelope-at mail-icon"></i>
+                <h3 className="contact-text">Send us an Email!</h3>
+              </a>
+              <p className="mail-text">Info</p>
+            </div>
           </div>
         </div>
         <p className="contact-confirmation">
